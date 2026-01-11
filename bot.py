366d3cd2f4df4ecd79c9e64fd8f90a66bd05d1b0
Leonardosignore <leonardosignore2004@gmail.com>
2026-01-11 22:10:26 +0100

    funziona

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ async def scegli_categoria(callback: CallbackQuery):
 
 @dp.callback_query(lambda c: c.data in ["prodotti"])
 async def get_all_prodotti_(callback: CallbackQuery):
-    await invia_lista_prodotti(callback)
+    await invia_lista_prodotti(callback.message)
 
 @dp.callback_query(lambda c: c.data.startswith("categoria:"))
 async def mostra_prodotti_categoria(callback: CallbackQuery):
